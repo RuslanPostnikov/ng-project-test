f1b22e9380975176bb568d82de86785f853b1341
RuslanPostnikov <rp02081992@gmail.com>
2022-02-15 12:04:56 +0200

    Heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {ProfilePageComponent} from "./profile-page/profile-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/login-page', pathMatch: 'full'},
   {path: 'login-page', component: LoginPageComponent},
   {path: 'home-page', component: HomePageComponent},
+  {path: 'profile-page', component: ProfilePageComponent},
 ];
 
 @NgModule({
